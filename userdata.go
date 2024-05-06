448c6b896a8aa5f3b214c5fb3d7202adc12b942f
Chris Trenkamp <chris@christrenkamp.com>
2024-05-05 20:20:11 -0400

    Remove unused equals helper method.

--- userdata.go
+++ userdata.go
@@ -13,21 +13,3 @@ func NewUserData(data LuaUserData, L *lua.LState) *lua.LUserData {
 		Metatable: L.GetTypeMetatable(data.LuaMetatableType()),
 	}
 }
-
-func UserDataEqual(l, r LuaUserData, L *lua.LState) bool {
-	eqFn := L.GetGlobal(l.LuaMetatableType()).(*lua.LTable).RawGet(lua.LString("__eq")).(*lua.LFunction)
-
-	err := L.CallByParam(lua.P{
-		Fn:   eqFn,
-		NRet: 1,
-	}, NewUserData(l, L), NewUserData(r, L))
-
-	if err != nil {
-		L.RaiseError("failed to compare objects: %s", err)
-	}
-
-	ret := L.Get(-1).(lua.LBool)
-	L.Pop(1)
-
-	return bool(ret)
-}
